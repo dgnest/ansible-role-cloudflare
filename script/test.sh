0736ed2521e00dae97238b071da975770599891c
@slovacus <slovacus@gmail.com>
2016-10-31 14:47:54 -0500

    [#7] Add: test script for ansible

--- script/test.sh
+++ script/test.sh
@@ -3,3 +3,6 @@
 
 # shellcheck source=script/bootstrap.sh
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
+
+ansible-playbook tests/test.yml -i tests/inventory --syntax-check
+ansible-playbook -i tests/inventory tests/test.yml --connection=local

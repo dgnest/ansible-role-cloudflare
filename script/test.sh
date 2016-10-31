bece006ff39f84b83772efd61533e7390a84c76c
@slovacus <slovacus@gmail.com>
2016-10-31 14:58:50 -0500

    [#9] Add: settings travis test

--- script/test.sh
+++ script/test.sh
@@ -4,5 +4,5 @@
 # shellcheck source=script/bootstrap.sh
 [ -r "script/bootstrap.sh" ] && source "script/bootstrap.sh"
 
-ansible-playbook tests/test.yml -i tests/inventory --syntax-check
-ansible-playbook -i tests/inventory tests/test.yml --connection=local
+ansible-playbook tests/test.yaml -i tests/inventory --syntax-check
+ansible-playbook -i tests/inventory tests/test.yaml --connection=local
